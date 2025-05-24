257980e26884e5411afe4c528cb8d002fc3b5d0e
DalaScript <nik.dalalishvili@gmail.com>
2025-05-24 19:21:12 +0400

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import styles from './Header.module.css'
-import HamburgerMenu from '../HamburgerMenu/HamburgerMenu'
+import React from 'react';
+import NavMenu from '../NavMenu/NavMenu';
+
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-        <HamburgerMenu />
+    <header>
+      <NavMenu />
     </header>
   )
 }
